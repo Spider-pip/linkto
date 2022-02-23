45c9789d96c4b98d27cb906487cea2c9b727c8ce
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2022-02-23 10:32:45 +0530

    fix: handle profile image uplaod

--- apps/studio/src/components/dashboard/PageProfileForm.tsx
+++ apps/studio/src/components/dashboard/PageProfileForm.tsx
@@ -178,7 +178,7 @@ const PageProfileForm: FC<PageProfileFormProps> = ({ page }) => {
                 </Avatar>
               ) : page.user.avatar_url ? (
                 <Avatar
-                  src={page.user.avatar_url}
+                  src={page.user.avatar_url + process.env.TOKEN}
                   title={avatarAlt}
                   alt={avatarAlt}
                   fallback={page.user.username}
