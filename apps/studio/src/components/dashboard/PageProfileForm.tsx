2c3765bf2e22307af4ace5e032adb9ea7e1bf29e
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2022-02-23 10:52:02 +0530

    fix: remove the fix

--- apps/studio/src/components/dashboard/PageProfileForm.tsx
+++ apps/studio/src/components/dashboard/PageProfileForm.tsx
@@ -178,7 +178,7 @@ const PageProfileForm: FC<PageProfileFormProps> = ({ page }) => {
                 </Avatar>
               ) : page.user.avatar_url ? (
                 <Avatar
-                  src={page.user.avatar_url +"/?token="+ process.env.TOKEN}
+                  src={page.user.avatar_url}
                   title={avatarAlt}
                   alt={avatarAlt}
                   fallback={page.user.username}
